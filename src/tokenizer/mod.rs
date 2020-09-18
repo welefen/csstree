8493d7976b59c1c96edb404f3151259910c66742
welefen <welefen@gmail.com>
2020-09-18 19:03:26 +0800

    code to char

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -6,22 +6,22 @@ pub mod tokenizer;
 
 // A code point between U+0030 DIGIT ZERO (0) and U+0039 DIGIT NINE (9) inclusive.
 pub fn is_digit(code: u8) -> bool {
-    code >= 0x30 && code <= 0x39
+    code >= b'0' && code <= b'9'
 }
 
 // A digit, or a code point between U+0041 LATIN CAPITAL LETTER A (A) and U+0046 LATIN CAPITAL LETTER F (F) inclusive, or a code point between U+0061 LATIN SMALL LETTER A (a) and U+0066 LATIN SMALL LETTER F (f) inclusive.
 pub fn is_hex_digit(code: u8) -> bool {
-    is_digit(code) || (code >= 0x0041 && code <= 0x0046) || (code >= 0x0061 && code <= 0x0066)
+    is_digit(code) || (code >= b'A' && code <= b'F') || (code >= b'a' && code <= b'f')
 }
 
 // A code point between U+0041 LATIN CAPITAL LETTER A (A) and U+005A LATIN CAPITAL LETTER Z (Z).
 pub fn is_uppercase_letter(code: u8) -> bool {
-    code >= 0x0041 && code <= 0x005A
+    code >= b'A' && code <= b'Z'
 }
 
 // A code point between U+0061 LATIN SMALL LETTER A (a) and U+007A LATIN SMALL LETTER Z (z).
 pub fn is_lowercase_ltter(code: u8) -> bool {
-    code >= 0x0061 && code <= 0x007A
+    code >= b'a' && code <= b'z'
 }
 
 // An uppercase letter or a lowercase letter.
@@ -36,12 +36,12 @@ pub fn is_non_ascii(code: u8) -> bool {
 
 // A letter, a non-ASCII code point, or U+005F LOW LINE (_).
 pub fn is_identifier_start(code: u8) -> bool {
-    is_letter(code) || is_non_ascii(code) || code == 0x005F
+    is_letter(code) || is_non_ascii(code) || code == b'_'
 }
 
 // An identifier-start code point, a digit, or U+002D HYPHEN-MINUS (-).
 pub fn is_identifier(code: u8) -> bool {
-    is_identifier_start(code) || is_digit(code) || code == 0x002D
+    is_identifier_start(code) || is_digit(code) || code == b'-'
 }
 // A code point between U+0000 NULL and U+0008 BACKSPACE inclusive, or U+000B LINE TABULATION, or a code point between U+000E SHIFT OUT and U+001F INFORMATION SEPARATOR ONE inclusive, or U+007F DELETE.
 pub fn is_non_printable(code: u8) -> bool {
@@ -88,6 +88,7 @@ pub fn would_start_a_number(first: u8, second: u8, third: u8) -> bool {
     return is_digit(first);
 }
 
+#[inline]
 pub fn utf8_is_cont_byte(byte: u8) -> bool {
     (byte & !0b0011_1111) == 0b1000_0000
 }
